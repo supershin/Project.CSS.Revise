e2b13531cda0bfbb2f57024386e8dbf088773301
SittikronPoti <firsty.shabby@gmail.com>
2025-03-11 16:58:36 +0700

    [commit & puch] - Layout site

--- Project.CSS.Revise.Web/Program.cs
+++ Project.CSS.Revise.Web/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Login}/{action=Index}/{id?}");
+    pattern: "{controller=Main}/{action=Index}/{id?}");
 
 app.Run();
