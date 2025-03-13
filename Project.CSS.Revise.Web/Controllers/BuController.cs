707e1a743560a281ea3cb4d2ba574a4329d2c005
SittikronPoti <firsty.shabby@gmail.com>
2025-03-13 17:03:46 +0700

    [commit & puch] - CSS page Bu and Project

---
+++ Project.CSS.Revise.Web/Controllers/BuController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project.CSS.Revise.Web.Controllers
+{
+    public class BuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Project.CSS.Revise.Web/Program.cs
+++ Project.CSS.Revise.Web/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Main}/{action=Index}/{id?}");
+    pattern: "{controller=Project}/{action=Index}/{id?}");
 
 app.Run();
