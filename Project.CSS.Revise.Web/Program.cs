68e1c54476a44227019bf67d3c7f5ae8671da2ea
SittikronPoti <firsty.shabby@gmail.com>
2025-03-18 10:52:19 +0700

    [commit & puch] - Appointment Limit page

--- Project.CSS.Revise.Web/Program.cs
+++ Project.CSS.Revise.Web/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=AppointmentLimit}/{action=Index}/{id?}");
+    pattern: "{controller=Login}/{action=Index}/{id?}");
 
 app.Run();
