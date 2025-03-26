e69640945679d09b5c8ae99fb8eda14d3d069b42
SittikronPoti <firsty.shabby@gmail.com>
2025-03-26 16:58:29 +0700

    [commit & push] User page have tab Project permission

---
+++ Project.CSS.Revise.Web/Controllers/CSResponseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project.CSS.Revise.Web.Controllers
+{
+    public class CSResponseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
