38152428567a5907f6cf94be3fb0314dd85f70db
SittikronPoti <firsty.shabby@gmail.com>
2025-04-03 15:36:05 +0700

    [commit & push] - UI User bank page

--- Project.CSS.Revise.Web/Program.cs
+++ Project.CSS.Revise.Web/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=CSMapping}/{action=Index}/{id?}");
+    pattern: "{controller=UserBank}/{action=Index}/{id?}");
 
 app.Run();
