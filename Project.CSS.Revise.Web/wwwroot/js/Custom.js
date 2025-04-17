8738a5d603912c1ee165de3bc7e2554c8dc015ac
SittikronPoti <firsty.shabby@gmail.com>
2025-04-17 17:10:07 +0700

    [commit & push] - Project & unit floor plan page have iamge

---
+++ Project.CSS.Revise.Web/wwwroot/js/Custom.js
@@ -0,0 +1,33 @@
+﻿let viewer;
+
+function openViewer(imageSrc) {
+
+    const viewerContainer = document.getElementById('imageViewerContainer');
+    const viewerImage = document.getElementById('viewerImage');
+
+    viewerImage.src = imageSrc;
+
+    if (viewer) {
+        viewer.destroy();
+    }
+
+    viewer = new Viewer(viewerContainer, {
+        inline: false,
+        navbar: false,
+        toolbar: {
+            zoomIn: 1,
+            zoomOut: 1,
+            oneToOne: 1,
+            reset: 1,
+            rotateLeft: 1,
+            rotateRight: 1,
+            flipHorizontal: 1,
+            flipVertical: 1,
+        },
+        hidden() {
+            viewerImage.src = ""; // clear image after close
+        }
+    });
+
+    viewer.show();
+}
