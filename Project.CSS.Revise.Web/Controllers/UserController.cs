e741e07d505d096091f0751c53187f9e9c672757
Sittikron Poti <sittikron.p@assetwise.co.th>
2025-09-20 11:38:40 +0700

    hot fix

--- Project.CSS.Revise.Web/Controllers/UserController.cs
+++ Project.CSS.Revise.Web/Controllers/UserController.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Project.CSS.Revise.Web.Service;
 
 namespace Project.CSS.Revise.Web.Controllers
 {
-    public class UserController : Controller
+    public class UserController : BaseController
     {
+        public UserController(IHttpContextAccessor httpContextAccessor) : base(httpContextAccessor)
+        {
+
+        }
+
         public IActionResult Index()
         {
             return View();
