b72bd8e11296a08d0340ca773bc8a97bce59b3a5
Sittikron Poti <sittikron.p@assetwise.co.th>
2025-10-15 10:10:30 +0700

    [commit] - Hot fix Summary card in Target Rolling

--- Project.CSS.Revise.Web/Library/DAL/MasterManagementProviderProject.cs
+++ Project.CSS.Revise.Web/Library/DAL/MasterManagementProviderProject.cs
@@ -275,7 +275,7 @@ namespace Project.CSS.Revise.Web.Library.DAL
             Entity.index = index;
             Entity.PlanTypeName = Commond.FormatExtension.NullToString(reader["PlanTypeName"]);
             Entity.PlanAmountName = Commond.FormatExtension.NullToString(reader["PlanAmountName"]);
-            Entity.TOTAL = Commond.FormatExtension.ConvertToShortUnithaveZero(reader["TOTAL"]);
+            Entity.TOTAL = Commond.FormatExtension.ConvertToShortUnitV2(reader["TOTAL"],Entity.PlanAmountName);
             Entity.COLORS = Commond.FormatExtension.NullToString(reader["COLORS"]);
 
             return Entity;
