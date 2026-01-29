c3ef10467fea7a199fd32978f891688870bd991c
sittikron.p <firsty.shabby@gmail.com>
2026-01-29 11:32:38 +0700

    Example DDL and Date Range Input.

--- Project.CSS.Revise.Web/Controllers/UnitController.cs
+++ Project.CSS.Revise.Web/Controllers/UnitController.cs
@@ -1,11 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Project.CSS.Revise.Web.Hubs;
+using Project.CSS.Revise.Web.Models.Master;
+using Project.CSS.Revise.Web.Service;
 
 namespace Project.CSS.Revise.Web.Controllers
 {
-    public class UnitController : Controller
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
+    public class UnitController : BaseController
     {
+        private readonly IMasterService _masterService;
+        private readonly IUserAndPermissionService _userAndPermissionService;
+
+        public UnitController(IHttpContextAccessor httpContextAccessor, IMasterService masterService, IUserAndPermissionService userAndPermissionService) : base(httpContextAccessor)
+        {
+            _masterService = masterService;
+            _userAndPermissionService = userAndPermissionService; ;
+        }
+
+
         public IActionResult Index()
         {
+            var listUnitstatuscs = _masterService.GetlisDDl(new GetDDLModel { Act = "Ext", ID = 16 });
+            ViewBag.listUnitstatuscs = listUnitstatuscs;
+
+            var listgCSRespons = _masterService.GetlisDDl(new GetDDLModel { Act = "listAllCSUser" });
+            ViewBag.listgCSRespons = listgCSRespons;
+
+            var allProject = _masterService.GetlisDDl(new GetDDLModel { Act = "listDDlAllProject" });
+            ViewBag.listProject = allProject;
+
             return View();
         }
     }
---
+++ Project.CSS.Revise.Web/wwwroot/js/Pages/Unit/Loading_DDL.js
@@ -0,0 +1,39 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const projectChoice = new Choices("#ddl_Project", {
+        searchEnabled: true,
+        removeItemButton: true, 
+        itemSelectText: "",
+        shouldSort: false,
+        placeholder: true,
+        placeholderValue: "Select project...",
+        allowHTML: false
+    });
+
+    const CSResponsChoice = new Choices("#ddl_CSReponse", {
+        searchEnabled: true,
+        removeItemButton: true,
+        itemSelectText: "",
+        shouldSort: false,
+        placeholder: true,
+        placeholderValue: "Select Cs reponse...",
+        allowHTML: false
+    });
+
+
+    fpRegisterStart = flatpickr("#txt_DateStart", {
+        dateFormat: "d/m/Y",
+        altInput: false,
+        allowInput: true,
+        onChange: function (selectedDates) {
+            if (fpRegisterEnd && selectedDates.length) {
+                fpRegisterEnd.set("minDate", selectedDates[0]);
+            }
+        }
+    });
+
+    fpRegisterEnd = flatpickr("#txt_DateEnd", {
+        dateFormat: "d/m/Y",
+        altInput: false,
+        allowInput: true,
+    });
+});
